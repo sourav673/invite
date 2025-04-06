47e171aa3fd79295362c1f718e02001f69ea98d6
sourav673 <sourav.kumar@privittytech.com>
2025-04-06 10:55:39 +0530

    customize branding and enable asset linking

--- i18n/en.js
+++ i18n/en.js
@@ -1,15 +1,15 @@
 Object.assign((typeof window === 'undefined' ? module.exports : window).i18n = (typeof window === 'undefined' ? module.exports : window).i18n || {}, {
   en: {
-    TitleText: 'Connect on Delta Chat',
-    DescriptionText: 'Delta Chat is a messaging app that works over e-mail.',
-    FormBlurbText: 'Create a Delta Chat invite link',
-    StepCodeText: 'Open Delta Chat, then navigate to <b>QR code → Show QR Invite Code → Copy to Clipboard</b>',
+    TitleText: 'Connect on Privitty Chat',
+    DescriptionText: 'Privitty Chat is a messaging app that works over e-mail.',
+    FormBlurbText: 'Create a Privitty Chat invite link',
+    StepCodeText: 'Open Privitty Chat, then navigate to <b>QR code → Show QR Invite Code → Copy to Clipboard</b>',
     StepPasteText: 'Paste into the following field:',
     StepShareText: 'Share the generated link over any channel',
     ShareLinkText: 'Open link',
     JoinText: 'Say hello to',
     JoinGroupText: 'Join',
     ChatText: 'Open chat',
-    DownloadText: 'Download Delta Chat',
+    DownloadText: 'Download Privitty Chat',
   },
 });
